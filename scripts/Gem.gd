d9aa41f935131016a09764e75bcb6d0dafc4e380
JpabloMF <juanpablo45671@gmail.com>
2020-11-01 19:06:35 -0500

    Gem animation and collision

--- scripts/Gem.gd
+++ scripts/Gem.gd
@@ -1,9 +1,28 @@
-extends Sprite
+extends Node2D
+
+export (String,FILE,"*.png") var diamond_sprite
+export (String,"blue","green","pink","yellow") var diamond_color = "blue"
+
+func load_diamond_animation():
+	var sprite
+	match diamond_color:
+		"blue":
+			sprite = load("res://animations/diamond/DiamondBlue.tres")
+		"green":
+			sprite = load("res://animations/diamond/DiamondGreen.tres")
+		"pink":
+			sprite = load("res://animations/diamond/DiamondPink.tres")
+		"yellow":
+			sprite = load("res://animations/diamond/DiamondYellow.tres")
+	$Diamond.set_sprite_frames(sprite)
 
 func _ready():
-	pass # Replace with function body.
+	load_diamond_animation()
 
 func _on_Coin_area_entered(area):
 	if "Player" in area.get_name():
-		queue_free()
+		remove_child($Diamond)
+		$collisionTimer.start()
 
+func _on_collisionTimer_timeout():
+	remove_child($Gem)
